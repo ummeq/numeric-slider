78828bf7ee2b82012a1a81fab61a78ecf7b7fd07
umme habiba <umme.a.qadri@gmail.com>
2020-12-27 12:32:12 +0530

    init commit

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
-// First let's set the colors of our sliders
-const settings={
-    fill: '#1abc9c',
-    background: '#d7dcdf'
+// set the colors of our sliders
+const settings = {
+  fill: "#1abc9c",
+  background: "#d7dcdf",
+};
+
+let ScreenValue;
+const sliderRange = document.querySelector("#slider-input");
+const sliderValue = document.querySelector("#slider-value");
+const sliders = document.querySelector(".range-slider__range");
+
+let isMobileScreen = false; // Mobile screen settings
+
+// Mobile screen condition
+isMobileScreen =
+  (screen.availWidth < 635 &&
+    (window.orientation === 0 || window.orientation === 180)) ||
+  (screen.availWidth < 900 &&
+    (window.orientation === -90 || window.orientation === 90));
+
+applyFill(sliders); // function to set background color
+sliders.addEventListener("input", (event) => {
+  document.querySelector(".range-slider__value").innerHTML = event.target.value;
+  applyFill(event.target);
+});
+
+
+function applyFill(slider) {
+  const percentage =
+    (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+  const bg = `linear-gradient(90deg, ${settings.fill} ${percentage}%, ${
+    settings.background
+  } ${percentage + 0.1}%)`;
+  sliders.style.background = bg;
+}
+
+const off =
+  sliderRange.offsetWidth /
+  (parseInt(sliderRange.max) - parseInt(sliderRange.min));
+if (isMobileScreen) {
+  ScreenValue = 70;
+} else {
+  ScreenValue = 130;
+}
+const px =
+  ScreenValue +
+  (sliderRange.valueAsNumber - parseInt(sliderRange.min)) * off -
+  sliderValue.offsetParent.offsetWidth / 2;
+
+sliderValue.parentElement.style.left = px + "px";
+sliderValue.parentElement.style.top = "35px";
+sliderValue.innerHTML = sliderRange.value;
+
+sliderRange.oninput = function () {
+  if (isMobileScreen) {
+    ScreenValue = 60;
+  } else {
+    ScreenValue = 100;
   }
-  
-    const sliders = document.querySelector('.range-slider__range');
-    applyFill(sliders);
-   // Array.prototype.forEach.call(sliders,(slider)=>{
-    sliders.addEventListener('input', (event)=>{
-        document.querySelector(".range-slider__value").innerHTML = event.target.value;
-        applyFill(event.target);
-        });
-        
-    //});
-
-    function applyFill(slider) {
-        const percentage = 100*(slider.value-slider.min)/(slider.max-slider.min);
-        const bg = `linear-gradient(90deg, ${settings.fill} ${percentage}%, ${settings.background} ${percentage+0.1}%)`;
-        sliders.style.background = bg;
-    }
-
-
-
-
-  //Code to set values 
-    var myRange = document.querySelector('#slider-input');
-    var myValue = document.querySelector('#slider-value');
-    var off = myRange.offsetWidth / (parseInt(myRange.max) - parseInt(myRange.min));
-    var px = 40 + ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetParent.offsetWidth / 2);
-
-    myValue.parentElement.style.left = px + 'px';
-    myValue.parentElement.style.top = '30px';
-    myValue.innerHTML = myRange.value ;
-
-    myRange.oninput =function(){
-    let px = ((myRange.valueAsNumber - parseInt(myRange.min)) * off) - (myValue.offsetWidth / 2);
-    myValue.innerHTML = myRange.value;
-    myValue.parentElement.style.left = px + 'px';
-    };
-
-    
-
-  
+  const px =
+    ScreenValue +
+    (sliderRange.valueAsNumber - parseInt(sliderRange.min)) * off -
+    sliderValue.offsetWidth / 2;
+  sliderValue.innerHTML = sliderRange.value;
+  sliderValue.parentElement.style.left = px + "px";
+};
